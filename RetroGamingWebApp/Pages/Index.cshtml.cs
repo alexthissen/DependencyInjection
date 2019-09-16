2b23b00d395f2cbf638ec0054192760401901120
Alex Thissen <athissen@xpirit.com>
2019-09-16 17:13:49 +0200

    Added retro gaming web app implementation

--- RetroGamingWebApp/Pages/Index.cshtml.cs
+++ RetroGamingWebApp/Pages/Index.cshtml.cs
@@ -1,25 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Polly.Timeout;
+using RetroGamingWebApp.Proxy;
 
 namespace RetroGamingWebApp.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ILogger<IndexModel> logger;
+        private readonly IOptionsSnapshot<LeaderboardApiOptions> options;
+        private readonly ILeaderboardClient proxy;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILeaderboardClient proxy, IOptionsSnapshot<LeaderboardApiOptions> options, ILoggerFactory loggerFactory)
         {
-            _logger = logger;
+            this.logger = loggerFactory.CreateLogger<IndexModel>();
+            this.options = options;
+            this.proxy = proxy;
         }
 
-        public void OnGet()
-        {
+        public IEnumerable<HighScore> Scores { get; private set; }
 
+        public async Task OnGetAsync()
+        {
+            Scores = new List<HighScore>();
+            try
+            {
+                //ILeaderboardClient proxy = RestService.For<ILeaderboardClient>(options.Value.BaseUrl);
+                Scores = await proxy.GetHighScores().ConfigureAwait(false);
+            }
+            catch (HttpRequestException ex)
+            {
+                logger.LogWarning(ex, "Http request failed.");
+            }
+            catch (TimeoutRejectedException ex)
+            {
+                logger.LogWarning(ex, "Timeout occurred when retrieving high score list.");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Unknown exception occurred while retrieving high score list");
+                throw;
+            }
         }
     }
 }
--- RetroGamingWebApp/Startup.cs
+++ RetroGamingWebApp/Startup.cs
@@ -1,13 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Polly;
+using Refit;
+using RetroGamingWebApp.Proxy;
 
 namespace RetroGamingWebApp
 {
@@ -23,11 +30,58 @@ namespace RetroGamingWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<LeaderboardApiOptions>(Configuration.GetSection("LeaderboardApiOptions"));
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            ConfigureTypedClients(services);
+            ConfigureSecurity(services);
+            ConfigureTelemetry(services);
+
             services.AddRazorPages();
         }
 
+        private void ConfigureTelemetry(IServiceCollection services)
+        {
+            services.AddSingleton<ITelemetryInitializer, ServiceNameInitializer>();
+            var env = services.BuildServiceProvider().GetRequiredService<IHostEnvironment>();
+            services.AddApplicationInsightsTelemetry(options =>
+            {
+                options.DeveloperMode = env.IsDevelopment();
+                options.InstrumentationKey = Configuration["ApplicationInsights:InstrumentationKey"];
+            });
+        }
+
+        private void ConfigureSecurity(IServiceCollection services)
+        {
+            services.AddHsts(
+                options =>
+                {
+                    options.MaxAge = TimeSpan.FromDays(100);
+                    options.IncludeSubDomains = true;
+                    options.Preload = true;
+                });
+        }
+
+        private void ConfigureTypedClients(IServiceCollection services)
+        {
+            services.AddHttpClient("WebAPIs", options =>
+            {
+                options.BaseAddress = new Uri(Configuration["LeaderboardApiOptions:BaseUrl"]);
+                options.Timeout = TimeSpan.FromMilliseconds(15000);
+                options.DefaultRequestHeaders.Add("ClientFactory", "Check");
+            })
+            .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromMilliseconds(1500)))
+            .AddTransientHttpErrorPolicy(p => p.RetryAsync(3))
+            .AddTypedClient(client => RestService.For<ILeaderboardClient>(client));
+        }
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
